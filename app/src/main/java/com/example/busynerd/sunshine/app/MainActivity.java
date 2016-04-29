95240974094f6d66f64a574156208521964d386d
busynerd <busynerd@gmail.com>
2016-04-29 14:33:22 -0700

    wrapping up lesson 2 from home

--- app/src/main/java/com/example/busynerd/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/busynerd/sunshine/app/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.busynerd.sunshine.app;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    // note: ActionBarActivity is deprecated, you should use AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
